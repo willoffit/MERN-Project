e2f082e417bcc5a60d8f30cdebc53c6ee59fd663
Will Offit <will.offit@gmail.com>
2020-08-31 14:37:14 -0700

    heroku

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ const passport = require("passport");
 const path = require("path");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+  app.use(express.static("frontend"));
   app.get("/", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "frontend", "app.js"));
   });
 }
 

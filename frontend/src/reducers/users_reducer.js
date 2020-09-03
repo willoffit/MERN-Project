644ee80495ebb284f6bd534562e3116c66561ff4
ldelapena <leah.d.delapena@gmail.com>
2020-09-03 12:22:37 -0700

    Add fixed users slice of state where key is userId

--- frontend/src/reducers/users_reducer.js
+++ frontend/src/reducers/users_reducer.js
@@ -2,10 +2,17 @@ import { RECEIVE_USERS } from '../actions/user_actions';
 
 const usersReducer = (oldState={}, action) => {
   Object.freeze(oldState);
+  let nextState = {};
 
   switch (action.type) {
     case RECEIVE_USERS:
-      return action.users;
+      // return action.users;
+      for (let i = 0; i < action.users.length; i++) {
+        let user = action.users[i];
+        nextState[user._id] = user
+      }
+
+      return nextState;
     default:
       return oldState;
   }
